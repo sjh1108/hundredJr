cb53c00fcdb5199e5779720eea1f983aa1bd5b14
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2023-01-09 18:52:15 +0900

    [Silver V] Title: 단어 정렬, Time: 344 ms, Memory: 26912 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        //Scanner s = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        
+        String[] arr = new String[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+           public int compare(String s1, String s2){
+            if(s1.length() == s2.length()){
+                return s1.compareTo(s2);
+            } else{
+                return s1.length() - s2.length();
+            }
+           } 
+        });
+
+        sb.append(arr[0]).append('\n');
+
+        for(int i = 1; i < N; i++){
+            if(!arr[i].equals(arr[i-1])){
+                sb.append(arr[i]).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+        //s.close();
+    }
+}
