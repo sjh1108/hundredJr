d3573a99d7222f8f0d394ab4519420ed225a4308
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2023-01-17 13:57:06 +0900

    [Gold IV] Title: 스도쿠, Time: 464 ms, Memory: 24280 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,95 +1,82 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	public static int[][] arr = new int[9][9];
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		for (int i = 0; i < 9; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
- 
-		sudoku(0, 0);
- 
-	}
- 
-	public static void sudoku(int row, int col) {
- 
-		// 해당 행이 다 채워졌을 경우 다음 행의 첫 번째 열부터 시작
-		if (col == 9) {
-			sudoku(row + 1, 0);
-			return;
-		}
- 
-		// 행과 열이 모두 채워졌을 경우 출력 후 종료
-		if (row == 9) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					sb.append(arr[i][j]).append(' ');
-				}
-				sb.append('\n');
-			}
-			System.out.println(sb);
-			// 출력 뒤 시스템을 종료한다.
-			System.exit(0);
-		}
- 
-		// 만약 해당 위치의 값이 0 이라면 1부터 9까지 중 가능한 수 탐색
-		if (arr[row][col] == 0) {
-			for (int i = 1; i <= 9; i++) {
-				// i 값이 중복되지 않는지 검사
-				if (possibility(row, col, i)) {
-					arr[row][col] = i;
-					sudoku(row, col + 1);
-				}
-			}
-			arr[row][col] = 0;
-			return;
-		}
- 
-		sudoku(row, col + 1);
- 
-	}
- 
-	public static boolean possibility(int row, int col, int value) {
- 
-		// 같은 행에 있는 원소들 중 겹치는 열 원소가 있는지 검사
-		for (int i = 0; i < 9; i++) {
-			if (arr[row][i] == value) {
-				return false;
-			}
-		}
- 
-		// 같은 열에 있는 원소들 중 겹치는 행 원소가 있는지 검사
-		for (int i = 0; i < 9; i++) {
-			if (arr[i][col] == value) {
-				return false;
-			}
-		}
- 
-		// 3*3 칸에 중복되는 원소가 있는지 검사
-		int set_row = (row / 3) * 3; // value가 속한 3x3의 행의 첫 위치
-		int set_col = (col / 3) * 3; // value가 속한 3x3의 열의 첫 위치
- 
-		for (int i = set_row; i < set_row + 3; i++) {
-			for (int j = set_col; j < set_col + 3; j++) {
-				if (arr[i][j] == value) {
-					return false;
-				}
-			}
-		}
- 
-		return true; // 중복되는 것이 없을 경우 true 반환
-	}
- 
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int M;
+
+    static int[][] arr = new int[9][9];
+    
+    static void sudoku(int x, int y){
+
+        if(y == 9){
+            sudoku(x + 1, 0);
+            return;
+        }
+        if(x == 9){
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    sb.append(arr[i][j]).append(' ');
+                }
+                sb.append('\n');
+            }
+            System.out.println(sb);
+            System.exit(0);
+        }
+    
+        if(arr[x][y] == 0){
+            for(int i = 1; i <= 9; i++){
+                if(possibility(x, y, i)){
+                    arr[x][y] = i;
+                    sudoku(x, y + 1);
+                }
+            }
+            arr[x][y] = 0;
+            return;
+        }
+
+        
+        sudoku(x, y + 1);
+    }
+
+    static boolean possibility(int x, int y, int n){
+        for(int i = 0; i < 9; i++){
+            if(arr[x][i] == n){
+                return false;
+            }
+
+            if(arr[i][y] == n){
+                return false;
+            }
+        }        
+
+        int nx = (x / 3) * 3;
+        int ny = (y / 3) * 3;
+
+        for(int i = nx; i < nx + 3; i++){
+            for(int j = ny; j < ny + 3; j++){
+                if(arr[i][j] == n){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException{
+        N = 0;
+        for(int i = 0; i < 9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < 9; j++){
+                int n = Integer.parseInt(st.nextToken());
+                arr[i][j] = n;
+            }
+        }
+        sudoku(0, 0);
+    }
 }
