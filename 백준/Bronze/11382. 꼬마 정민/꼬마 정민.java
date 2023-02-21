f811709beaf39bcc0adf89a494b7c515b40cd337
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2023-02-21 12:11:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N;
+    static int M;
+    static int K;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A + B + C);
+    }
+}
