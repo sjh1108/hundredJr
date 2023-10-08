287a70ddf12630734a9e4657785b443925dba823
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2023-10-08 16:15:22 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 260 ms, Memory: 17340 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) throws IOException{
+        N = Integer.parseInt(br.readLine());
+
+        int[][] mat = new int[501][2];
+        int[][] dp = new int[501][501];
+
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            mat[i][0] = Integer.parseInt(st.nextToken());
+            mat[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j <= N - i; j++){
+                int k = i + j;
+                dp[j][k] = INF;
+
+                for(int l = j; l < k; l++){
+                    dp[j][k] = Math.min(dp[j][k], dp[j][l] + dp[l + 1][k] + mat[j][0] * mat[l][1] * mat[k][1]);
+                }
+            }
+        }
+
+        System.out.println(dp[1][N]);
+        // System.out.println(sb);
+    }
+}
