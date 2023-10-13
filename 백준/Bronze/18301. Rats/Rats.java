91462da17b2d69adc9ce8e353a9ecc9872d5d4af
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2023-10-13 16:46:29 +0900

    [Bronze V] Title: Rats, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        a = (a + 1) * (b + 1) / (c + 1) - 1;
+
+        System.out.println(a);
+    }
+}
