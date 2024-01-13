f2acd25e4efc605273b5746ef2ea913a3ae657ec
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2024-01-13 19:56:42 +0900

    [Bronze I] Title: 2007년, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+        
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        int[] months = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        
+        int n = 0;
+        for (int i = 0; i < m; i++) 
+            n += months[i];
+            
+        n += d-1;
+        System.out.print(days[n%7]);
+    }
+}
