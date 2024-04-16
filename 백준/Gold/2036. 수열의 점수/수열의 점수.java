64d3cea6b80989bb7bcc397f3cd71d69d1e94df4
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2024-04-16 19:03:44 +0900

    [Gold IV] Title: 수열의 점수, Time: 516 ms, Memory: 36880 KB -BaekjoonHub

---
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException{
+        N = Integer.parseInt(br.readLine());
+
+		BigInteger answer=new BigInteger("0");
+		
+		ArrayList<Long> pList = new ArrayList<Long>();
+		ArrayList<Long> mList = new ArrayList<Long>();
+        int oCnt = 0;
+		for(int i = 0; i < N; i++)
+		{
+			long n = Long.parseLong(br.readLine());
+			if(n > 1)
+				pList.add(n);
+			else if(n < 1)
+				mList.add(n);
+			else
+                oCnt++;
+		}
+		
+		Collections.sort(pList, Collections.reverseOrder());
+		Collections.sort(mList);
+		
+		if(pList.size() % 2 == 0) {
+			for(int i = 0; i < pList.size(); i++)
+				answer = answer.add(BigInteger.valueOf(pList.get(i) * pList.get(i++ + 1)));
+		}
+		
+		else {
+			for(int i = 0; i < pList.size() - 1; i++)
+				answer = answer.add(BigInteger.valueOf(pList.get(i) * pList.get(i++ + 1)));
+			answer = answer.add(BigInteger.valueOf(pList.get(pList.size()-1)));
+		}
+		
+		if(mList.size() % 2 == 0)
+			for(int i = 0; i < mList.size(); i++)
+				answer = answer.add(BigInteger.valueOf(mList.get(i) * mList.get(i++ + 1)));
+		
+		else {
+			for(int i = 0; i < mList.size()-1; i++)
+				answer = answer.add(BigInteger.valueOf(mList.get(i) * mList.get(i++ + 1)));
+			answer=answer.add(BigInteger.valueOf(mList.get(mList.size() - 1)));
+		}
+		
+		answer = answer.add(BigInteger.valueOf(oCnt));
+		System.out.println(answer);
+    }
+}
