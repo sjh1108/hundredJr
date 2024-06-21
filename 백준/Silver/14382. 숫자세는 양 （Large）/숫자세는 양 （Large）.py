6ceea8c0e0920ce2e0267b50366d131352c75ba3
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2024-06-21 18:23:20 +0900

    [Silver IV] Title: 숫자세는 양 (Large), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14382. 숫자세는 양 （Large）/숫자세는 양 （Large）.py
@@ -0,0 +1,22 @@
+t = int(input())
+
+for i in range(t):
+    check = {}
+    n = int(input())
+    now = n
+    if n == 0:
+        print('Case #'+str(i+1)+": INSOMNIA")
+    else:
+        count = 1
+        while True:
+            temp = str(now)
+            for j in temp:
+                if int(j) not in check:
+                    check[j] = 1
+                else:
+                    continue
+            if sum(check.values()) == 10:
+                print("Case #"+str(i+1)+ ": "+str(now)) # , 로 하면 공백열이 생겨버리므로 + 를 이용해서 문자열을 만들어야 함
+                break
+            count += 1
+            now = n * count
