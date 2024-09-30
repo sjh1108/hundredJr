5c765e34f8c7bcf598b7bf1a4df78efd64511df2
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2024-09-30 11:30:55 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 432 ms, Memory: 61832 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M, K;
+    
+    static Stack<Node> stack = new Stack<>();
+
+    static class Node{
+        int height;
+        int count;
+
+        Node(int height, int count){
+            this.height = height;
+            this.count = count;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        N = Integer.parseInt(br.readLine());
+
+        long count = 0;
+        for(int i = 0; i < N; i++){
+            Node node = new Node(Integer.parseInt(br.readLine()), 1);
+
+            while(!stack.isEmpty() && stack.peek().height <= node.height){
+                Node tmp = stack.pop();
+                count += tmp.count;
+                if(tmp.height == node.height){
+                    node.count += tmp.count;
+                }
+            }
+
+            if(!stack.isEmpty()){
+                count++;
+            }
+
+            stack.push(node);
+        }
+
+        System.out.println(count);
+    }
+}
