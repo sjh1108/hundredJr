75c92fe89f00953e1017f54015960eadeee0ec28
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-06-20 19:51:17 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 120 ms, Memory: 18176 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        int[] fib = new int[n + 2];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = (fib[i - 1] + fib[i - 2]) % 1000000007;
+        }
+        
+        System.out.println(fib[n]);
+    }
+}
