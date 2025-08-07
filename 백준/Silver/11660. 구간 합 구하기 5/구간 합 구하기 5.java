224031a8dd656c2ee525cf73055fb37498fc03ba
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-07 14:17:06 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1016 ms, Memory: 114848 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,54 +1,39 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int N;
-    static int M;
-    static int max;
-    
-    static int[] arr;
-    static int[] srr;
-
-    static long[] f;
-    static long[] lrr;
-
-    static int[][] dp;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        dp = new int[N + 1][N + 1];
-
-        for(int i = 1; i < N + 1; i++){
+        int[][] arr = new int[N+1][N+1];
+        for(int i = 1; i < N+1; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j < N + 1; j++){
-                dp[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] += dp[i][j - 1];
+            for(int j = 1; j < N+1; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i][j-1];
             }
         }
 
-        long sum = 0;
-
-        for(int i = 0; i < M; i++){
+        StringBuilder sb = new StringBuilder();
+        while(M--> 0){
             st = new StringTokenizer(br.readLine());
 
-            sum = 0;
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
-            int xgap = c - a;
-            
-            for(int j = a; j <= xgap + a; j++){
-                sum += func(j, b, d);
+            int sum = 0;
+
+            for(int i = x1; i <= x2; i++){
+                sum += arr[i][y2] - arr[i][y1-1];
             }
 
             sb.append(sum);
@@ -57,9 +42,4 @@ public class Main {
 
         System.out.println(sb);
     }
-
-    static int func(int x, int i, int j){
-        return dp[x][j] - dp[x][i - 1];
-    }
-
 }
