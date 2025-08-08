c85c0a9d6790cb7d6d1476e152771e3d91c8c735
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-08 17:30:10 +0900

    [Gold III] Title: 줄 세우기, Time: 532 ms, Memory: 43552 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,69 +1,83 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    static int N, M;
-
-    static int max = 0;
-
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i = 0; i <= N; i++) list.add(new ArrayList<>());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        for(int i = 0; i < M; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> list = new ArrayList<>(N+1);
+
+        // list 초기화
+        for(int i = 0; i <= N; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        // 우선순위 입력
+        while(M-- > 0){
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             list.get(a).add(b);
         }
 
+        // 방문한 노드
         boolean[] visited = new boolean[N+1];
-        ArrayList<Integer> result = new ArrayList<>();
+
+        // 줄 세우기
+        int[] line = new int[N];
+        int cnt = 0;
         for(int i = 1; i <= N; i++){
-            if(list.get(i).size() == 0) {
-                result.add(i);
+            if(list.get(i).size() == 0){
+                line[cnt++] = i;
                 visited[i] = true;
             }
         }
-        
-        while(result.size() < N){
+
+        // list에 노드가 없을때까지
+        while(cnt < N){
+            
             for(int i = 1; i <= N; i++){
                 if(visited[i]) continue;
 
                 if(list.get(i).size() == 0){
-                    result.add(i);
                     visited[i] = true;
+                    line[cnt++] = i;
                     continue;
                 }
 
                 if(list.get(i).size() == 1){
                     if(visited[list.get(i).get(0)]){
-                        result.add(i);
+                        line[cnt++] = i;
                         visited[i] = true;
                         list.get(i).remove(0);
                     }
                 } else{
                     for(int j = 0; j < list.get(i).size(); j++){
-                        if(visited[list.get(i).get(j)]) {
-                            list.get(i).remove(j);
+                        if(visited[list.get(i).get(j)]){
+                            list.get(i).remove(j--);
                         }
                     }
                 }
             }
         }
 
-        for(int i = result.size() - 1; i >= 0; i--){
-            sb.append(result.get(i)).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            int x = line[--cnt];
+            sb.append(x).append(" ");
         }
+
         System.out.println(sb);
     }
 }
