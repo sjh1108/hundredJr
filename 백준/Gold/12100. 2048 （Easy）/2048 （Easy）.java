4fb78154bd5461cbaa0eaded320f7c0f2b66354c
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-13 23:02:36 +0900

    [Gold I] Title: 2048 (Easy), Time: 184 ms, Memory: 22868 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,186 +1,91 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+public class Main {
+    private static int N;
 
-    static int N, M;
+    private static int max = 0;
 
-	static int max = 0;
-
-	static class Map{
-		int[][] map;
-		int cnt;
-        public Map(int[][] map){
-            this(map, 0);
-        }
-		public Map(int[][] map, int cnt){
-            this.map = new int[N][N];
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    this.map[i][j] = map[i][j];
-                }
+    // 맵의 최대값을 저장하기 위한 메소드
+    private static void getMax(int[][] map) {
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                max = Math.max(max, map[i][j]);
             }
-			this.cnt = cnt;
-		}
-
-		public int getMax(){
-			int max = 0;
-			for(int i = 0; i < N; i++){
-				for(int j = 0; j < N; j++){
-					max = Math.max(max, map[i][j]);
-				}
-			}
-			return max;
-		}
-
-		public int[][] goLeft(){
-            int[][] tmp = new int[N][N];
-            
-            for(int i = 0; i < N; i++){
-                int[] t = new int[N];
-
-                int idx = 0;
-                for(int j = 0; j < N; j++){
-                    if(map[i][j] == 0) continue;
-
-                    if(t[idx] == 0){
-                        t[idx] = map[i][j];
-                    }
-                    else if(t[idx] == map[i][j]){
-                        t[idx] *= 2;
-                        idx++;
-                    }else{
-                        idx++;
-                        t[idx] = map[i][j];
-                    }
-                }
-
-                tmp[i] = t.clone();
-            }
-
-            return tmp;
-		}
-        public int[][] goRight(){
-            int[][] tmp = new int[N][N];
-            
-            for(int i = 0; i < N; i++){
-                int[] t = new int[N];
-
-                int idx = N-1;
-                for(int j = N-1; j >= 0; j--){
-                    if(map[i][j] == 0) continue;
-
-                    if(t[idx] == 0){
-                        t[idx] = map[i][j];
-                    }
-                    else if(t[idx] == map[i][j]){
-                        t[idx] *= 2;
-                        idx--;
-                    }else{
-                        idx--;
-                        t[idx] = map[i][j];
-                    }
-                }
-
-                tmp[i] = t.clone();
-            }
-
-            return tmp;
-        }
-        public int[][] goUp(){
-            int[][] tmp = new int[N][N];
-            
-            for(int i = 0; i < N; i++){
-                int[] t = new int[N];
-
-                int idx = 0;
-                for(int j = 0; j < N; j++){
-                    if(map[j][i] == 0) continue;
-
-                    if(t[idx] == 0){
-                        t[idx] = map[j][i];
-                    }
-                    else if(t[idx] == map[j][i]){
-                        t[idx] *= 2;
-                        idx++;
-                    }else{
-                        idx++;
-                        t[idx] = map[j][i];
-                    }
-                }
-
-                for(int j = 0; j < N; j++){
-                    tmp[j][i] = t[j];
-                }
-            }
-
-            return tmp;
-        }
-        public int[][] goDown(){
-            int[][] tmp = new int[N][N];
-            
-            for(int i = 0; i < N; i++){
-                int[] t = new int[N];
-
-                int idx = N-1;
-                for(int j = N-1; j >= 0; j--){
-                    if(map[j][i] == 0) continue;
-
-                    if(t[idx] == 0){
-                        t[idx] = map[j][i];
-                    }
-                    else if(t[idx] == map[j][i]){
-                        t[idx] *= 2;
-                        idx--;
-                    }else{
-                        idx--;
-                        t[idx] = map[j][i];
-                    }
-                }
-
-                for(int j = 0; j < N; j++){
-                    tmp[j][i] = t[j];
-                }
-            }
-
-            return tmp;
         }
     }
 
-    public static void main(String[] args) throws IOException{
-		N = Integer.parseInt(br.readLine());
+    private static int[][] rotate(int[][] map) {
+        int[][] rotated = new int[N][N];
 
-		int[][] map = new int[N][N];
-		for(int i = 0; i < N; i++){
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++){
-				map[i][j] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, map[i][j]);
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                rotated[j][N - 1 - i] = map[i][j];
+            }
+        }
 
-		bfs(map);
+        return rotated;
+    }
 
-		System.out.println(max);
-	}
+    private static void pressMap(int[][] map){
+        for(int i = 0; i < N; i++){
+            int[] t = new int[N];
 
-	static void bfs(int[][] map){
-		Queue<Map> q = new LinkedList<>();
-		q.offer(new Map(map));
+            int idx = 0;
+            for(int j = 0; j < N; j++){
+                if(map[j][i] == 0) continue;
 
-		while(!q.isEmpty()){
-			Map cur = q.poll();
-			if(cur.cnt == 5){
-				max = Math.max(max, cur.getMax());
-				continue;
-			}
-			q.offer(new Map(cur.goLeft(),   cur.cnt+1));
-			q.offer(new Map(cur.goRight(),  cur.cnt+1));
-			q.offer(new Map(cur.goUp(),     cur.cnt+1));
-			q.offer(new Map(cur.goDown(),   cur.cnt+1));
-		}
-	}
+                if(t[idx] == 0){
+                    t[idx] = map[j][i];
+                }
+                else if(t[idx] == map[j][i]){
+                    t[idx] *= 2;
+                    idx++;
+                }else{
+                    idx++;
+                    t[idx] = map[j][i];
+                }
+            }
+
+            for(int j = 0; j < N; j++){
+                map[j][i] = t[j];
+            }
+        }
+    }
+
+    private static void dfs(int[][] map, int depth){
+        if(depth == 5) {
+            getMax(map);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int[][] newMap = rotate(map);
+
+            pressMap(newMap);
+            dfs(newMap, depth + 1);
+
+            map = rotate(map);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        
+        int[][] map = new int[N][N];
+        
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(map, 0);
+
+        System.out.println(max);
+    }
 }
