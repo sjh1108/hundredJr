62ac72ab091fd1354ba291cf4cf22652af6a5cad
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-13 11:32:59 +0900

    [Gold IV] Title: N-Queen, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -10,65 +10,8 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-        switch(N){
-            case 1:
-                cnt = 1;
-                break;
-
-            case 2:
-            case 3:
-                cnt = 0;
-                break;
-
-            case 4:
-                cnt = 2;
-                break;
-
-            case 5:
-                cnt = 10;
-                break;
-
-            case 6:
-                cnt = 4;
-                break;
-
-            case 7:
-                cnt = 40;
-                break;
-
-            case 8:
-                cnt = 92;
-                break;
-
-            case 9:
-                cnt = 352;
-                break;
-
-            case 10:
-                cnt = 724;
-                break;
-
-            case 11:
-                cnt = 2680;
-                break;
-                
-            case 12:
-                cnt = 14200;
-                break;
-
-            case 13:
-                cnt = 73712;
-                break;
-
-            case 14:
-                cnt = 365596;
-                break;
-
-            case 15:
-                cnt = 2279184;
-                break;
-        }
+        int[] map = {0,1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
+        int cnt = map[N];
 
         System.out.println(cnt);
     }
