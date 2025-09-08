979c8c96674e0afb85082ff9dbd7c1dc12a33806
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-09-08 15:23:32 +0900

    [Gold V] Title: 최소비용 구하기, Time: 476 ms, Memory: 50956 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,74 +2,73 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+    private static int N, M;
 
-    static int N, M;
+    private static int[] dist;
+    private static List<List<Edge>> graph = new ArrayList<>();
+    private static Queue<Edge> pq = new PriorityQueue<>();
 
-    static ArrayList<ArrayList<Node>> graph;
-    static int INF = 100000000;
-    static int[] dist;
-    static int ans = 0;
+    private static class Edge implements Comparable<Edge>{
+        int from, to, weight;
 
-    static class Node implements Comparable<Node>{
-        int end, cost;
-        Node(int end, int cost){
-            this.end = end;
-            this.cost = cost;
+        public Edge(int from, int to, int weight){
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
         }
 
         @Override
-        public int compareTo(Node o){
-            return this.cost - o.cost;
+        public int compareTo(Edge o){
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+
+    private static void dijkstra(int start, int end){
+        dist[start] = 0;
+        pq.offer(new Edge(-1, start, 0));
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+
+            if(dist[cur.to] < cur.weight) continue;
+
+            for(Edge next : graph.get(cur.to)){
+                if(dist[next.to] > dist[cur.to] + next.weight){
+                    dist[next.to] = dist[cur.to] + next.weight;
+                    pq.offer(new Edge(next.from, next.to, dist[next.to]));
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
 
-        dist = new int[N + 1];
-        Arrays.fill(dist, INF);
         graph = new ArrayList<>();
-        for(int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+        for(int i=0; i<N; i++) graph.add(new ArrayList<>());
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken())   - 1;
+            int weight = Integer.parseInt(st.nextToken());
 
-        while(M-- > 0){
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph.get(start).add(new Node(end, cost));
+            graph.get(from).add(new Edge(from, to, weight));
         }
+
+        dist = new int[N];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
         st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
+        int from = Integer.parseInt(st.nextToken()) - 1;
+        int to = Integer.parseInt(st.nextToken()) - 1;
 
-        dijkstra(start, end);
-        System.out.println(ans);
-    }
+        dijkstra(from, to);
 
-    static void dijkstra(int start, int end){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
-
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-
-            if(dist[cur.end] < cur.cost) continue;
-
-            for(Node next : graph.get(cur.end)){
-                if(dist[next.end] > cur.cost + next.cost){
-                    dist[next.end] = cur.cost + next.cost;
-                    pq.offer(new Node(next.end, dist[next.end]));
-                }
-            }
-        }
-
-        ans = dist[end];
+        System.out.println(dist[to]);
     }
 }
