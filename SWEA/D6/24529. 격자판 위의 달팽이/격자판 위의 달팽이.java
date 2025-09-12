8e6d8817bf5e3d6f6fb9089654dc019ec0313a47
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-09-12 10:37:07 +0900

    [D6] Title: 격자판 위의 달팽이, Time: 3,041 ms, Memory: 106,240 KB -BaekjoonHub

--- SWEA/D6/24529. 격자판 위의 달팽이/격자판 위의 달팽이.java
+++ SWEA/D6/24529. 격자판 위의 달팽이/격자판 위의 달팽이.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.BitSet;
 
 public class Solution {
-
-    static final int MAX_SUM = 25 * 50000 + 1;
+    static final int MAX_N = 25;
+    static final int MAX_A_VAL = 50000;
+    static final int MAX_SUM = (MAX_N - 1) * 2 * MAX_A_VAL;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
+        
         for (int tc = 1; tc <= T; ++tc) {
             solve(br, sb);
         }
@@ -32,12 +32,10 @@ public class Solution {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 1. 숫자들을 오름차순으로 정렬
         Arrays.sort(A);
 
-        // 2. 가장 작은 두 수를 제외한 나머지 배열 생성
         int[] arr = new int[2 * N - 2];
-        int totalSum = 0;
+        long totalSum = 0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = A[i + 2];
             totalSum += arr[i];
@@ -45,70 +43,126 @@ public class Solution {
 
         int halfN = N - 1;
         
-        // 3. DP를 위한 BitSet 배열 초기화
-        // dp[count]는 count개의 숫자로 만들 수 있는 합의 집합
-        BitSet[] dp = new BitSet[halfN + 1];
+        Bignum[] dp = new Bignum[halfN + 1];
         for (int i = 0; i <= halfN; i++) {
-            dp[i] = new BitSet(totalSum + 1);
+            dp[i] = new Bignum(MAX_SUM);
         }
-        dp[0].set(0); // 0개의 숫자로 합 0을 만들 수 있음
+        dp[0].setBit(0);
 
-        // 4. DP 실행
+        Bignum temp = new Bignum(MAX_SUM); // 임시 객체 재사용으로 오버헤드 최소화
         for (int val : arr) {
             for (int cnt = halfN; cnt >= 1; cnt--) {
-                // C++의 dp[cnt] |= dp[cnt-1] << val; 와 동일한 로직
-                // dp[cnt-1]에 포함된 모든 합(i)에 대해, dp[cnt]에 (i + val)을 추가
-                BitSet shifted = new BitSet(totalSum + 1);
-                for (int i = dp[cnt - 1].nextSetBit(0); i >= 0; i = dp[cnt - 1].nextSetBit(i + 1)) {
-                    shifted.set(i + val);
-                }
-                dp[cnt].or(shifted);
+                // dp[cnt] |= (dp[cnt - 1] << val)
+                temp.shiftLeft(dp[cnt - 1], val);
+                dp[cnt].or(temp);
             }
         }
         
-        // 5. 최적의 부분합 찾기 (전체 합의 절반에 가장 가까운)
         int bestSum = 0;
-        int halfSum = totalSum / 2;
-        for (int i = halfSum; i >= 0; --i) {
-            if (dp[halfN].get(i)) {
+        long halfSum = totalSum / 2;
+        for (int i = (int)halfSum; i >= 0; --i) {
+            if (dp[halfN].getBit(i)) {
                 bestSum = i;
                 break;
             }
         }
 
-        // 6. 역추적을 통해 두 그룹 생성
         List<Integer> group1 = new ArrayList<>();
         List<Integer> group2 = new ArrayList<>();
         
         int currentSum = bestSum;
         int currentCount = halfN;
+        boolean[] used = new boolean[arr.length];
 
         for (int i = arr.length - 1; i >= 0; i--) {
             int val = arr[i];
-            if (currentCount > 0 && currentSum >= val && dp[currentCount - 1].get(currentSum - val)) {
-                group1.add(val);
+            if (currentCount > 0 && currentSum >= val && dp[currentCount - 1].getBit(currentSum - val)) {
+                used[i] = true;
                 currentSum -= val;
                 currentCount--;
-            } else {
-                group2.add(val);
             }
         }
 
-        // 7. 출력 형식에 맞게 정렬
+        for (int i = 0; i < arr.length; i++) {
+            if (used[i]) {
+                group1.add(arr[i]);
+            } else {
+                group2.add(arr[i]);
+            }
+        }
+
         Collections.sort(group1);
-        Collections.sort(group2, Collections.reverseOrder());
+        group2.sort(Collections.reverseOrder());
 
-        // 8. 결과 출력
-        sb.append(A[0]).append(' ');
-        for (int val : group1) {
-            sb.append(val).append(' ');
-        }
+        sb.append(A[0]);
+        for (int val : group1) sb.append(' ').append(val);
         sb.append('\n');
 
-        for (int val : group2) {
-            sb.append(val).append(' ');
+        for (int i = 0; i < group2.size(); i++) sb.append(group2.get(i)).append(' ');
+        sb.append(A[1]).append('\n');
+    }
+
+    // Bignum 클래스 (위에 정의된 내용)
+    static class Bignum {
+        final long[] words;
+        final static int WORD_BITS = 64;
+
+        public Bignum(int size) {
+            this.words = new long[(size / WORD_BITS) + 1];
         }
-        sb.append(A[1]).append(' ');
-        sb.append('\n');
+
+        public void setBit(int bit) {
+            int wordIdx = bit / WORD_BITS;
+            int bitIdx = bit % WORD_BITS;
+            if (wordIdx < words.length) {
+                words[wordIdx] |= (1L << bitIdx);
+            }
+        }
+
+        public boolean getBit(int bit) {
+            int wordIdx = bit / WORD_BITS;
+            int bitIdx = bit % WORD_BITS;
+            if (wordIdx >= words.length) return false;
+            return ((words[wordIdx] >> bitIdx) & 1) == 1;
+        }
+
+        public void or(Bignum other) {
+            int n = Math.min(this.words.length, other.words.length);
+            for (int i = 0; i < n; i++) {
+                this.words[i] |= other.words[i];
+            }
+        }
+        
+        public void clear() {
+            Arrays.fill(words, 0L);
+        }
+
+        public void shiftLeft(Bignum source, int shift) {
+            this.clear();
+            
+            int wordShift = shift / WORD_BITS;
+            int bitShift = shift % WORD_BITS;
+
+            if (bitShift == 0) {
+                for (int i = 0; i < source.words.length; i++) {
+                    int destIdx = i + wordShift;
+                    if (destIdx < this.words.length) {
+                        this.words[destIdx] = source.words[i];
+                    }
+                }
+            } else {
+                for (int i = 0; i < source.words.length; i++) {
+                    int destIdx1 = i + wordShift;
+                    if (destIdx1 < this.words.length) {
+                        this.words[destIdx1] |= (source.words[i] << bitShift);
+                    }
+                    
+                    int destIdx2 = i + wordShift + 1;
+                    if (destIdx2 < this.words.length) {
+                        this.words[destIdx2] |= (source.words[i] >>> (WORD_BITS - bitShift));
+                    }
+                }
+            }
+        }
     }
 }
