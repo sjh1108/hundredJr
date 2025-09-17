a7adace37239bacd346e5ee161da0db4d3c52a9e
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-09-17 20:48:22 +0900

    [Gold I] Title: 구간 합 구하기, Time: 560 ms, Memory: 103284 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -2,53 +2,53 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    private static int N, M, K;
-
-    private static long[] arr, tree;
+    private static long[] arr;
+    private static long[] tree;
 
     private static long init(int start, int end, int node){
         if(start == end) return tree[node] = arr[start];
 
         int mid = (start + end) / 2;
 
-        return tree[node] = init(start, mid, node * 2) + init(mid+1, end, node*2 + 1);
+        return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2 + 1);
     }
 
-    private static long sum(int start, int end, int node, int left, int right){
-        if(left > end || right < start) return 0;
-
-        if(left <= start && end <= right){
-            return tree[node];
-        }
-
-        int mid = (start + end) / 2;
-        return sum(start, mid, node * 2, left, right) + sum(mid+1, end, node*2 + 1, left, right);
-    }
-
-    private static void update(int start, int end, int node, int idx, long dif){
-        if(idx < start || idx > end) return;
+    private static void update(int start, int end, int node, int index, long dif){
+        if(index < start || index > end) return;
 
         tree[node] += dif;
         if(start == end) return;
 
         int mid = (start + end) / 2;
 
-        update(start, mid, node*2, idx, dif);
-        update(mid+1, end, node*2+1, idx, dif);
+        update(start, mid, node * 2, index, dif);
+        update(mid+1, end, node*2+1, index, dif);
     }
+
+    private static long sum(int start, int end, int node, int left, int right){
+        if(left > end || right < start) return 0;
+
+        if(left <= start && end <= right) return tree[node];
+
+        int mid = (start + end) / 2;
+
+        return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         arr = new long[N+1];
 
-        int tmp = (int)Math.ceil(Math.log(N) / Math.log(2)) + 1;
-        int size = (int)(Math.pow(2, tmp));
+        int k = (int)Math.ceil(Math.log(N) / Math.log(2)) + 1;
+        int size = (int)Math.pow(2, k);
+        
         tree = new long[size];
 
         for(int i = 1; i <= N; i++){
@@ -62,22 +62,26 @@ class Main {
             st = new StringTokenizer(br.readLine());
 
             int cmd = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
 
             if(cmd == 1){
                 M--;
+                int index = Integer.parseInt(st.nextToken());
+                long n = Long.parseLong(st.nextToken());
 
-                long dif = b - arr[a];
-                arr[a] = b;
-
-                update(1, N, 1, a, dif);
+                long dif = n - arr[index];
+                arr[index] = n;
+                
+                update(1, N, 1, index, dif);
             } else{
                 K--;
-                sb.append(sum(1, N, 1, a, (int) b) + "\n");
+
+                int left = Integer.parseInt(st.nextToken());
+                int right = Integer.parseInt(st.nextToken());
+
+                sb.append(sum(1, N, 1, left, right) + "\n");
             }
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
