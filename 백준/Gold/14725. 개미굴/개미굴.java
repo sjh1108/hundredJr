07025de1aa742766d9484b4f150d7172160be8f5
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-09-23 23:19:40 +0900

    [Gold III] Title: 개미굴, Time: 208 ms, Memory: 18640 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static class Node{
+        Map<String, Node> children;
+        public Node(){
+            this.children = new HashMap<>();
+        }
+    }
+    private static int N;
+    private static Node root;
+    private static StringBuilder sb;
+
+    private static void print(Node root, int depth){
+        PriorityQueue<String> pq = new PriorityQueue<>(root.children.keySet());
+        String depthString = "";
+
+        for(int i = 0; i < depth; i++){
+            depthString += "--";
+        }
+
+        while(!pq.isEmpty()){
+            String cur = pq.poll();
+
+            sb.append(depthString + cur + "\n");
+            print(root.children.get(cur), depth+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        root = new Node();
+        sb = new StringBuilder();
+
+        StringTokenizer st;
+        while(N-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+
+            Node cur = root;
+            while(K-- > 0){
+                String leaf = st.nextToken();
+
+                if(!cur.children.containsKey(leaf)){
+                    cur.children.put(leaf, new Node());
+                }
+                cur = cur.children.get(leaf);
+            }
+        }
+
+        print(root, 0);
+
+        System.out.println(sb);
+    }
+}
