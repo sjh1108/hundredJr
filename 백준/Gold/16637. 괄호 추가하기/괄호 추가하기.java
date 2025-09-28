9765187fec4795ee6198b5ea424004911e056145
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-09-28 19:12:47 +0900

    [Gold III] Title: 괄호 추가하기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -2,11 +2,38 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int ans;
-    static ArrayList<Character> ops;
-    static ArrayList<Integer> nums;
+    private static int ans;
+    private static ArrayList<Character> ops;
+    private static ArrayList<Integer> nums;
  
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    private static int calc(char op, int n1, int n2) {
+        switch (op) {
+        case '+':
+            return n1 + n2;
+        case '-':
+            return n1 - n2;
+        case '*':
+            return n1 * n2;
+        }
+        return -1;
+    }
+    private static void dfs(int result, int idx) {
+        if (idx == ops.size()) {
+            ans = Math.max(ans, result);
+            return;
+        }
+ 
+        // 괄호가 없는 경우
+        int res = calc(ops.get(idx), result, nums.get(idx + 1));
+        dfs(res, idx + 1);
+ 
+        // 괄호가 있는 경우
+        if (idx + 1 < ops.size()) {
+            res = calc(ops.get(idx + 1), nums.get(idx + 1), nums.get(idx + 2));
+            dfs(calc(ops.get(idx), result, res), idx + 2);
+        }
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
@@ -27,39 +54,6 @@ class Main {
         dfs(nums.get(0), 0);
  
         System.out.println(ans);
-    }
- 
-    // 연산
-    public static int calc(char op, int n1, int n2) {
-        switch (op) {
-        case '+':
-            return n1 + n2;
-        case '-':
-            return n1 - n2;
-        case '*':
-            return n1 * n2;
-        }
-        return -1;
-    }
- 
-    public static void dfs(int result, int idx) {
-        if (idx == ops.size()) {
-            ans = Math.max(ans, result);
-            return;
-        }
- 
-        // 괄호가 없는 경우
-        int res = calc(ops.get(idx), result, nums.get(idx + 1));
-        dfs(res, idx + 1);
- 
-        // 괄호가 있는 경우
-        if (idx + 1 < ops.size()) {
-            // result의 오른쪽에 있는 값을 연산함.
-            res = calc(ops.get(idx + 1), nums.get(idx + 1), nums.get(idx + 2));
- 
-            // 현재 result와 방금 구한 괄호 값을 연산한 결과와 괄호 오른쪽에 존재하는 연산자의 인덱스를 넘김.
-            dfs(calc(ops.get(idx), result, res), idx + 2);
-        }
     }
  
 }
