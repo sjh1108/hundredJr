078fcbd48326d28eb2d0da5398440367a7bcbe9b
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-10-15 23:28:26 +0900

    [Silver I] Title: 출근, Time: 560 ms, Memory: 93816 KB -BaekjoonHub

--- 백준/Silver/13903. 출근/출근.java
+++ 백준/Silver/13903. 출근/출근.java
@@ -10,11 +10,12 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        Queue<int[]> q = new ArrayDeque<>();
+        // 맵 초기화
         int[][] map = new int[R][C];
         for(int i = 0; i < R; i++){
             st = new StringTokenizer(br.readLine());
@@ -22,12 +23,8 @@ class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i = 0; i < C; i++){
-            if(map[0][i] == 1){
-                q.add(new int[]{0, i});
-            }
-        }
 
+        // 명령어 입력
         int c = Integer.parseInt(br.readLine());
         int[][] cmdArray = new int[c][2];
         for(int i = 0; i < c; i++){
@@ -36,9 +33,18 @@ class Main {
             cmdArray[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        // 시작지점 초기화
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i = 0; i < C; i++){
+            if(map[0][i] == 1){
+                q.add(new int[]{0, i});
+            }
+        }
+
         int cnt = 0;
         boolean[][] visited = new boolean[R][C];
         while(!q.isEmpty()){
+            // 반복 수 설정을 위한 Queue의 size 계산
             int size = q.size();
 
             while(size-- > 0){
@@ -46,9 +52,6 @@ class Main {
 
                 int x = cur[0];
                 int y = cur[1];
-
-                if(visited[x][y]) continue;
-                visited[x][y] = true;
 
                 if(cur[0] == R-1){
                     System.out.println(cnt);
@@ -58,8 +61,13 @@ class Main {
                     int nx = x + cmd[0];
                     int ny = y + cmd[1];
 
+                    // 맵 밖으로 나가면 탐색하지 않음
                     if(!isIn(nx, ny)) continue;
+
+                    // 가로 블럭 || 방문한 위치면 탐색하지 않음
                     if(map[nx][ny] == 0 || visited[nx][ny]) continue;
+                    // 방문처리
+                    visited[nx][ny] = true;
 
                     q.add(new int[]{nx, ny});
                 }
