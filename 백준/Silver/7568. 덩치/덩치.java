87a71636f9333f8dcb62e7a2664ab5638ad76304
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-10-15 23:40:06 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,33 +1,34 @@
-//import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+        int[][] person = new int[N][2];
         
-        int[][] p = new int[N][3];
+        StringTokenizer st;
         for(int i = 0; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            p[i][0] = Integer.parseInt(st.nextToken());
-            p[i][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            person[i][0] = Integer.parseInt(st.nextToken());
+            person[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < N; i++){
             int rank = 1;
 
             for(int j = 0; j < N; j++){
                 if(i == j) continue;
-                if(p[i][0] < p[j][0] && p[i][1] < p[j][1]){
+                if(person[i][0] < person[j][0] && person[i][1] < person[j][1]){
                     rank++;
                 }
             }
 
             sb.append(rank).append(' ');
         }
-        
+
         System.out.println(sb);
     }
 }
