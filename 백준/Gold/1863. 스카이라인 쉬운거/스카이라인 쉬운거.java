375d893a41925963cd964dfde7fdbf1e57a595b7
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-10-21 17:09:30 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 120 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -3,34 +3,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-    static int N, answer;
-	static int[] stack;
-	static int top;
 
+    // static 변수들을 제거하고 main 내의 지역 변수로 모두 처리
+	
 	public static void main(String[] args) throws Exception {
-		// 입력 및 초기화
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int answer = 0; // 0을 미리 넣어두고 시
-		int top = 0; // 0이 들어있으니 top은 0번째 인덱스
+		
+        // N도 main 내의 지역 변수로 선언
+        int N = Integer.parseInt(br.readLine());
+		
+        // answer와 top도 지역 변수로 선언
+        int answer = 0;
+		int top = 0; // 스택의 현재 위치(인덱스)를 가리킴
 
-		stack = new int[N + 1]; // 들어갈 수 있는 최대 크기 N
+        // 스택 배열도 지역 변수로 선언
+        // 스택의 바닥(stack[0])을 0으로 사용하기 위해 N+1 크기 할당
+		int[] stack = new int[N + 1]; 
 
-		// 입력 받으면서 정답 처리
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			st.nextToken();
-			int num = Integer.parseInt(st.nextToken());
-			// stack top이 num 보다 크면 뺀다.
+			st.nextToken(); // 첫 번째 토큰 (x좌표)은 무시
+			int num = Integer.parseInt(st.nextToken()); // 두 번째 토큰 (y좌표)
+			
+            // 스택의 top에 있는 높이가 현재 높이(num)보다 높으면
+            // 낮아질 때까지 pop (건물 종료)
 			while (stack[top] > num) {
 				top--;
-				answer++;
+				answer++; // pop할 때마다 완료된 건물로 카운트
 			}
-			// top이 작으면 넣는다
+
+			// 스택의 top이 현재 높이(num)보다 낮으면 push (새 건물 시작)
+            // (num이 0이고 stack[top]도 0이면 push되지 않음)
 			if (stack[top] < num) {
 				stack[++top] = num;
 			}
+            
+            // stack[top] == num 이면 아무것도 하지 않음 (건물 이어짐)
 		}
+        
+        // answer = 중간에 pop된 건물 수
+        // top = 마지막까지 스택에 남아있는 건물 수 (stack[0] 제외)
 		System.out.println(answer + top);
 
 	}
