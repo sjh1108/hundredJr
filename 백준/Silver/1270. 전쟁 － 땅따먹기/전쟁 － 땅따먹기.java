30793b730600165c5daf4f406a1db34b2826c0ba
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-10-22 17:41:23 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 2592 ms, Memory: 306048 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
@@ -4,53 +4,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer tokens;
-    static int N, Pnum;
-    static long[] peoples;
-
-    public static void main(String[] args) {
-        try {
-            N = Integer.parseInt(input.readLine());
-
-            for (int i = 0; i < N; i++) {
-                tokens = new StringTokenizer(input.readLine());
-                Pnum = Integer.parseInt(tokens.nextToken());
-                peoples = new long[Pnum];
-
-                for (int j = 0; j < Pnum; j++) {
-                    peoples[j] = Long.parseLong(tokens.nextToken());
-                }
-
-                long findNum = 0;
-                int cnt = 0;
-                for (int j = 0; j < Pnum; j++) {
-                    if (cnt == 0) {
-                        findNum = peoples[j];
-                        cnt = 1;
-                    } else if (peoples[j] == findNum) {
-                        cnt++;
-                    } else {
-                        cnt--;
-                    }
-                }
-
-                // 과반수인지 확인
-                int realCnt = 0;
-                for (int j = 0; j < Pnum; j++) {
-                    if (peoples[j] == findNum) {
-                        realCnt++;
-                    }
-                }
-
-                if (realCnt > Pnum / 2) {
-                    System.out.println(findNum);
-                } else {
-                    System.out.println("SYJKGW");
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int N,Pnum;
+	static long[] peoples;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(input.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			tokens = new StringTokenizer(input.readLine());
+			Pnum = Integer.parseInt(tokens.nextToken());
+			peoples = new long [Pnum];
+			
+			for (int j = 0; j < Pnum; j++) {
+				peoples[j] = Long.parseLong(tokens.nextToken());
+			}
+			
+			long findNum = 0;
+			int cnt = 0;
+			for (int j = 0; j < Pnum; j++) {
+				if(cnt == 0) {
+					findNum = peoples[j];
+					cnt = 1;
+				}else if(peoples[j] == findNum) {
+					cnt++;
+				}else {
+					cnt--;
+				}
+			}
+			// 병사수 확인후 과반수이면 병사 출력 아니면 "SYJKGW" 출력
+			int realCnt = 0;
+            for (int j = 0; j < Pnum; j++) {
+                if (peoples[j] == findNum) {
+                	realCnt++;
                 }
             }
-        } catch (IOException | NumberFormatException e) {
-            System.out.println("입력 오류 발생: " + e.getMessage());
-        }
-    }
+            if (realCnt > Pnum / 2) {
+                System.out.println(findNum);
+            } else {
+                System.out.println("SYJKGW");
+            }
+			
+		} //tc
+		
+		
+	}
 }
