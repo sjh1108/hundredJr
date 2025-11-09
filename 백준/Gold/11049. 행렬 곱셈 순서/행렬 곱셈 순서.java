788892c960ee4145961d1b60a028b4a8ab7da030
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-11-09 16:45:14 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 224 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,24 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    static int N, M;
-
+class Main{
     public static void main(String[] args) throws IOException{
-        N = Integer.parseInt(br.readLine());
-
-        int[][] mat = new int[501][2];
-        int[][] dp = new int[501][501];
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] map = new int[N+1][2];
+        int[][] dp = new int[N+1][N+1];
+        
+        StringTokenizer st;
         for(int i = 1; i <= N; i++){
             st = new StringTokenizer(br.readLine());
 
-            mat[i][0] = Integer.parseInt(st.nextToken());
-            mat[i][1] = Integer.parseInt(st.nextToken());
+            map[i][0] = Integer.parseInt(st.nextToken());
+            map[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 1; i < N; i++){
@@ -27,12 +23,11 @@ public class Main{
                 dp[j][k] = Integer.MAX_VALUE;
 
                 for(int l = j; l < k; l++){
-                    dp[j][k] = Math.min(dp[j][k], dp[j][l] + dp[l + 1][k] + mat[j][0] * mat[l][1] * mat[k][1]);
+                    dp[j][k] = Math.min(dp[j][k], dp[j][l] + dp[l + 1][k] + map[j][0] * map[l][1] * map[k][1]);
                 }
             }
         }
 
         System.out.println(dp[1][N]);
-        // System.out.println(sb);
     }
 }
