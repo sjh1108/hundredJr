cb44bf2bc06bfbf3d294a679d972099edcaadcbe
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-12-07 20:04:16 +0900

    [Gold I] Title: 트리의 순회, Time: 1412 ms, Memory: 59156 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,16 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    // static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int N, M, K;
+class Main {
 
     static int idx = 0;
     static int[] inOrder, postOrder;
     static List<Integer> preOrder = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        inOrder = new int[N];
+        postOrder = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inOrder[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            postOrder[i] = Integer.parseInt(st.nextToken());
+        }
+
+        getPreOrder(0, N-1, 0, N-1);
+
+        StringBuilder sb = new StringBuilder();
+        for(int n : preOrder) {
+            sb.append(n + " ");
+        }
+        System.out.println(sb);
+    }
 
     static void getPreOrder(int is, int ie, int ps, int pe) {
         if(is > ie || ps > pe) return;
@@ -27,27 +48,5 @@ public class Main {
 
         getPreOrder(is, root-1, ps, ps+root-is-1);
         getPreOrder(root+1, ie, ps+root-is, pe-1);
-    }
-
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-
-        inOrder = new int[N];
-        postOrder = new int[N];
-        
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            inOrder[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            postOrder[i] = Integer.parseInt(st.nextToken());
-        }
-
-        getPreOrder(0, N-1, 0, N-1);
-        for(int n : preOrder) {
-            sb.append(n + " ");
-        }
-        System.out.println(sb);
     }
 }
