36567844155ac652f5b741edfc616e4b54431bd5
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-02-12 11:18:31 +0900

    [Gold I] Title: 귀농, Time: 180 ms, Memory: 33752 KB -BaekjoonHub

--- 백준/Gold/1184. 귀농/귀농.java
+++ 백준/Gold/1184. 귀농/귀농.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
     private static int N;
     private static int[][] arr;
     private static int[][] psum;
+    
+    // N이 최대 30, 값이 -1000~1000 범위이므로 합의 범위는 넉넉잡아 설정
+    private static final int OFFSET = 2000000;
+    private static int[] countArr = new int[OFFSET * 2 + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,42 +30,60 @@ class Main {
 
         for (int i = 1; i < N; i++) {
             for (int j = 1; j < N; j++) {
-
-                Map<Integer, Integer> map = new HashMap<>();
                 
-                // \
+                // ------------------------------------------------
+                // Case 1: \ 방향 (왼쪽 위 -> 오른쪽 아래)
+                // ------------------------------------------------
+                
+                // 1. 왼쪽 위 영역 (기록)
                 for (int r = 1; r <= i; r++) {
                     for (int c = 1; c <= j; c++) {
                         int sum = getSum(r, c, i, j);
-                        map.put(sum, map.getOrDefault(sum, 0) + 1);
+                        countArr[sum + OFFSET]++; // 배열에 카운팅
                     }
                 }
 
+                // 2. 오른쪽 아래 영역 (확인 및 정답 누적)
                 for (int r = i + 1; r <= N; r++) {
                     for (int c = j + 1; c <= N; c++) {
                         int sum = getSum(i + 1, j + 1, r, c);
-                        if (map.containsKey(sum)) {
-                            ans += map.get(sum);
-                        }
+                        ans += countArr[sum + OFFSET]; // 배열에서 O(1) 조회
                     }
                 }
 
-                map.clear();
+                // 3. 배열 초기화 (중요: 전체 초기화가 아니라 기록했던 곳만 되돌림)
+                for (int r = 1; r <= i; r++) {
+                    for (int c = 1; c <= j; c++) {
+                        int sum = getSum(r, c, i, j);
+                        countArr[sum + OFFSET]--; // 0으로 원상복구
+                    }
+                }
+
+                // ------------------------------------------------
+                // Case 2: / 방향 (오른쪽 위 -> 왼쪽 아래)
+                // ------------------------------------------------
                 
-                // /
+                // 1. 오른쪽 위 영역 (기록)
                 for (int r = 1; r <= i; r++) {
                     for (int c = j + 1; c <= N; c++) {
                         int sum = getSum(r, j + 1, i, c);
-                        map.put(sum, map.getOrDefault(sum, 0) + 1);
+                        countArr[sum + OFFSET]++;
                     }
                 }
 
+                // 2. 왼쪽 아래 영역 (확인 및 정답 누적)
                 for (int r = i + 1; r <= N; r++) {
                     for (int c = 1; c <= j; c++) {
                         int sum = getSum(i + 1, c, r, j);
-                        if (map.containsKey(sum)) {
-                            ans += map.get(sum);
-                        }
+                        ans += countArr[sum + OFFSET];
+                    }
+                }
+                
+                // 3. 배열 초기화
+                for (int r = 1; r <= i; r++) {
+                    for (int c = j + 1; c <= N; c++) {
+                        int sum = getSum(r, j + 1, i, c);
+                        countArr[sum + OFFSET]--;
                     }
                 }
             }
