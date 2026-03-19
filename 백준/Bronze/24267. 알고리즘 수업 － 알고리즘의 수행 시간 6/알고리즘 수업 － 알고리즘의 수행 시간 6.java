cfee8d0582efdd194972dbf4f58841d10753d3b3
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-03-19 20:57:43 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        long a = Long.parseLong(br.readLine());
+
+        System.out.println(a * (a - 1) * (a - 2) / 6);
+        System.out.println(3);
+    }
+}
