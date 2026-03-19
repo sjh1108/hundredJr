6816d9ae96bd7f23549a0da1ff7fd8de495c8cb1
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-03-19 20:59:24 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, M, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        N = Integer.parseInt(br.readLine());
+
+        int n = (int)(Math.pow(2, N));
+
+        System.out.println(n);
+    }
+}
