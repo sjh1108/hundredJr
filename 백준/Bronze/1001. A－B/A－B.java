c2d727fa971cd5e92ec2aa2743e968442b17117c
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-03-19 21:04:44 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        a -= s.nextInt();
+        System.out.print(a);
+    }
+}
