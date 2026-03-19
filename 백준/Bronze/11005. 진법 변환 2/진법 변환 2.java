92af20e8b2fe9b5340a19cc47dd5745838406c61
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-03-19 20:54:53 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        List<Character> list = new ArrayList<>();
+        while(n > 0){
+            if(n % b < 10){
+                list.add((char) (n % b + '0'));
+            } else{
+                list.add((char) (n % b - 10 + 'A'));
+            }
+
+            n /= b;
+        }
+
+        for(int i = list.size() - 1; i >= 0; i--){
+            System.out.print(list.get(i));
+        }
+    }
+}
