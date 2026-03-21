7de158ee998a9b94c2e993e326b8e325ba0ad6c8
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2026-03-22 00:45:44 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 376 ms, Memory: 40404 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -14,21 +14,21 @@ class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
-        dp = new int[N][N];
+        map = new int[N+2][N+2];
+        dp = new int[N+2][N+2];
 
         StringTokenizer st;
-        for(int i = 0; i < N; i++){
+        for(int i = 1; i <= N; i++){
             st = new StringTokenizer(br.readLine());
 
-            for(int j = 0; j < N; j++){
+            for(int j = 1; j <= N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int ans = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++) {
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++) {
                 ans = Math.max(ans, dfs(i, j));
             }
         }
@@ -39,22 +39,16 @@ class Main {
     private static int dfs(int x, int y){
         if(dp[x][y] > 0) return dp[x][y];
 
-        dp[x][y] = 1;
-
         int max = 0;
         for(int d = 0; d < 4; d++){
             int nx = x + dx[d];
             int ny = y + dy[d];
 
-            if(!isIn(nx, ny)) continue;
-            if(map[nx][ny] <= map[x][y]) continue;
-
-            if(dfs(nx, ny) > max) max = dp[nx][ny];
+            if(map[nx][ny] > map[x][y]) {
+                max = Math.max(max, dfs(nx, ny));
+            }
         }
 
-        return dp[x][y] = dp[x][y] + max;
-    }
-    private static boolean isIn(int x, int y){
-        return x >= 0 && x < N && y >= 0 && y < N;
+        return dp[x][y] = max + 1;
     }
 }
