20066790e13e2225e1e9d3f90b404d48d7683bd5
송주헌 <ch6854@naver.com>
2026-04-15 00:53:57 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 180 ms, Memory: 59800 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] inputs = new String[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            inputs[i] = st.nextToken();
+        }
+        Arrays.sort(inputs, new Comparator<String>() {
+            public int compare(String o1, String o2){
+                BigInteger b1 = new BigInteger(o1 + o2);
+                BigInteger b2 = new BigInteger(o2 + o1);
+
+                return b2.compareTo(b1);
+            }
+        });
+
+        String ans = "";
+        for(String str: inputs) ans += str;
+
+        if(ans.charAt(0) == '0')
+            System.out.println(0);
+        else
+            System.out.println(ans);
+    }
+}
