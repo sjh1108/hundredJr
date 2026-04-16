0bfb5ad8e9d861f619f7d1e045870ed01f2067f4
송주헌 <ch6854@naver.com>
2026-04-16 10:02:26 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1452 ms, Memory: 151920 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -5,67 +5,70 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
 
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        while (m-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken()) - 1;
-            int v = Integer.parseInt(st.nextToken()) - 1;
-            int w = Integer.parseInt(st.nextToken());
-            graph.get(u).add(new int[]{v, w});
-        }
-
+        List<List<int[]>> edge = new ArrayList<>();
         List<List<Integer>> dist = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i <= N; i++){
+            edge.add(new ArrayList<>());
             dist.add(new ArrayList<>());
         }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
         
-        pq.add(new int[]{0, 0});
-        dist.get(0).add(0);
+        while(M -- > 0){
+            st = new StringTokenizer(br.readLine());
 
-        while (!pq.isEmpty()) {
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            edge.get(a).add(new int[]{b, d});
+        }
+
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        pq.add(new int[]{1, 0});
+        dist.get(1).add(0);
+
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
+
             int u = cur[0];
-            int cost = cur[1];
+            int v = cur[1];
 
-            for (int[] next : graph.get(u)) {
-                int v = next[0];
-                int w = next[1];
-                int newCost = cost + w;
-                
-                List<Integer> vDist = dist.get(v);
+            for(int[] next: edge.get(u)){
+                int nu = next[0];
+                int nv = v + next[1];
 
-                if (vDist.size() < k) {
-                    vDist.add(newCost);
-                    Collections.sort(vDist);
-                    pq.add(new int[]{v, newCost});
-                } 
-                else {
-                    int maxDist = vDist.get(vDist.size() - 1);
-                    
-                    if (newCost < maxDist) {
-                        vDist.remove(vDist.size() - 1);
-                        vDist.add(newCost);
-                        Collections.sort(vDist);
-                        pq.add(new int[]{v, newCost});
+                List<Integer> nDist = dist.get(nu);
+
+                if(nDist.size() < K){
+                    nDist.add(nv);
+                    Collections.sort(nDist);
+                    pq.add(new int[]{nu, nv});
+                } else{
+                    int max = nDist.get(nDist.size() - 1);
+
+                    if(nv < max){
+                        nDist.remove(nDist.size() - 1);
+                        nDist.add(nv);
+                        Collections.sort(nDist);
+                        pq.add(new int[]{nu, nv});
                     }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(List<Integer> d : dist) {
-            if (d.size() == k)  sb.append(d.get(k - 1)).append('\n');
-            else                sb.append(-1).append('\n');
+        for(int i = 1; i <= N; i++){
+            if(dist.get(i).size() < K){
+                sb.append(-1);
+            } else{
+                sb.append(dist.get(i).get(K-1));
+            }
+            sb.append('\n');
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
