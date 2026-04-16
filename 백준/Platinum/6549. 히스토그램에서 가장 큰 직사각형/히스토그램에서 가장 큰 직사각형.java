8b10156cd1cf29aea522e1a58128cb0f6f82458a
송주헌 <ch6854@naver.com>
2026-04-16 20:47:22 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 372 ms, Memory: 55772 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -1,39 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+class Main {
+    private static int[] arr;
 
-    static int N;
-    static int M;
-    static int K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
 
-    static int[] arr;
-
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-
+        StringBuilder sb = new StringBuilder();
         while(N != 0){
             arr = new int[N];
             for(int i = 0; i < N; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            sb.append(getArea(0, N - 1));
+            sb.append(getArea(0, N-1));
             sb.append('\n');
-            
+
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
         }
 
         System.out.println(sb);
-
     }
 
-    static long getArea(int lo, int hi){
+    private static long getArea(int lo, int hi){
         if(lo == hi){
             return arr[lo];
         }
@@ -50,7 +44,7 @@ public class Main {
         return max;
     }
 
-    static long getMidArea(int lo, int mid, int hi){
+    private static long getMidArea(int lo, int mid, int hi){
         int toLeft = mid;
         int toRight = mid;
 
@@ -70,7 +64,6 @@ public class Main {
             maxArea = Math.max(maxArea, height * (toRight - toLeft + 1));
         }
 
-
         while(toRight < hi){
             toRight++;
             height = Math.min(height, arr[toRight]);
@@ -82,7 +75,6 @@ public class Main {
             height = Math.min(height, arr[toLeft]);
             maxArea = Math.max(maxArea, height * (toRight - toLeft + 1));
         }
-
 
         return maxArea;
     }
