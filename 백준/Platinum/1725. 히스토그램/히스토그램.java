8440d0b80694837d5b24be98f8595c69840071fa
송주헌 <ch6854@naver.com>
2026-04-16 20:44:07 +0900

    [Platinum V] Title: 히스토그램, Time: 248 ms, Memory: 24636 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.java
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -1,21 +1,15 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+class Main {
+    private static int[] arr;
 
-    static int N;
-    static int M;
-    static int K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
 
-    static int[] arr;
-
-    public static void main(String[] args) throws IOException{
-        N = Integer.parseInt(br.readLine());
         arr = new int[N];
-
         for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
@@ -23,7 +17,7 @@ public class Main {
         System.out.println(getArea(0, N - 1));
     }
 
-    static long getArea(int lo, int hi){
+    private static long getArea(int lo, int hi){
         if(lo == hi){
             return arr[lo];
         }
@@ -40,7 +34,7 @@ public class Main {
         return max;
     }
 
-    static long getMidArea(int lo, int mid, int hi){
+    private static long getMidArea(int lo, int mid, int hi){
         int toLeft = mid;
         int toRight = mid;
 
@@ -72,7 +66,6 @@ public class Main {
             height = Math.min(height, arr[toLeft]);
             maxArea = Math.max(maxArea, height * (toRight - toLeft + 1));
         }
-
 
         return maxArea;
     }
