b920cc49670c3419ec138d2f3377e75f95c2a78d
송주헌 <ch6854@naver.com>
2026-04-21 22:47:30 +0900

    [Gold I] Title: 구간 합 구하기, Time: 500 ms, Memory: 119584 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -5,36 +5,6 @@ class Main {
     private static long[] arr;
     private static long[] tree;
 
-    private static long init(int start, int end, int node){
-        if(start == end) return tree[node] = arr[start];
-
-        int mid = (start + end) / 2;
-
-        return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2 + 1);
-    }
-
-    private static void update(int start, int end, int node, int index, long dif){
-        if(index < start || index > end) return;
-
-        tree[node] += dif;
-        if(start == end) return;
-
-        int mid = (start + end) / 2;
-
-        update(start, mid, node * 2, index, dif);
-        update(mid+1, end, node*2+1, index, dif);
-    }
-
-    private static long sum(int start, int end, int node, int left, int right){
-        if(left > end || right < start) return 0;
-
-        if(left <= start && end <= right) return tree[node];
-
-        int mid = (start + end) / 2;
-
-        return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -83,5 +53,35 @@ class Main {
         }
 
         System.out.println(sb);
+    }
+
+    private static long init(int start, int end, int node){
+        if(start == end) return tree[node] = arr[start];
+
+        int mid = (start + end) / 2;
+
+        return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2 + 1);
+    }
+
+    private static void update(int start, int end, int node, int index, long dif){
+        if(index < start || index > end) return;
+
+        tree[node] += dif;
+        if(start == end) return;
+
+        int mid = (start + end) / 2;
+
+        update(start, mid, node * 2, index, dif);
+        update(mid+1, end, node*2+1, index, dif);
+    }
+
+    private static long sum(int start, int end, int node, int left, int right){
+        if(left > end || right < start) return 0;
+
+        if(left <= start && end <= right) return tree[node];
+
+        int mid = (start + end) / 2;
+
+        return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
     }
 }
