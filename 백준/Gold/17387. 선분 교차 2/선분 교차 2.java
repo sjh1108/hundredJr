f2b6bdc4ae38147fe6c8f7c78cef101727d3a44e
송주헌 <ch6854@naver.com>
2026-04-25 00:10:30 +0900

    [Gold II] Title: 선분 교차 2, Time: 60 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
@@ -1,13 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-
-    static int N, M, K;
-    
+class Main{
     static class Point{
         long x, y;
         Point(long x, long y){
@@ -17,8 +11,10 @@ public class Main{
     }
 
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Point[] arr = new Point[4];
 
+        StringTokenizer st;
         for(int i = 0; i < 4; i+=2){
             st = new StringTokenizer(br.readLine());
             arr[i] = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
@@ -36,7 +32,6 @@ public class Main{
 		int p341 = ccw(p[2], p[3], p[0]);
 		int p342 = ccw(p[2], p[3], p[1]);
 
-        // 두 직선이 일직선 상에 있음
 		if (p123 * p124 == 0 && p341 * p342 == 0) {
 			is_result = true;
 
@@ -45,14 +40,11 @@ public class Main{
             boolean compare2 = Math.min(p[2].x, p[3].x) <= Math.max(p[0].x, p[1].x);
             boolean compare3 = Math.min(p[0].y, p[1].y) <= Math.max(p[2].y, p[3].y);
             boolean compare4 = Math.min(p[2].y, p[3].y) <= Math.max(p[0].y, p[1].y);
-            // 두 선이 겹침
 			if (compare1 && compare2 && compare3 && compare4)
 				result = 1;
 		}
 
-        // 두 직선이 일직선 상에 없으나, 한 점이 겹치는 경우를 고려하기 위해 <= 0으로 체크함
 		if (p123 * p124 <= 0 && p341 * p342 <= 0) {
-            // 일직선 상이 아니라면
 			if (!is_result)
 				result = 1;
 		}
@@ -60,16 +52,13 @@ public class Main{
 		return result;
 	}
 
-    public static int ccw(Point p1, Point p2, Point p3){
+    private static int ccw(Point p1, Point p2, Point p3){
         long result = ((p1.x * p2.y) + (p2.x * p3.y) + (p3.x * p1.y)) - ((p1.y * p2.x) + (p2.y * p3.x) + (p3.y * p1.x));
 
-        // 반시계
 		if (result > 0)
 			return 1;
-        // 일직선
 		else if (result == 0)
 			return 0;
-        // 시계
 		else
 			return -1;
     }
