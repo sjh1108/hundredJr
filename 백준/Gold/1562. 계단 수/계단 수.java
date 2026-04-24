fa7c26014b3007da093d53d2e6d1328ba4a9d81d
송주헌 <ch6854@naver.com>
2026-04-24 23:54:08 +0900

    [Gold I] Title: 계단 수, Time: 108 ms, Memory: 20644 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,17 +1,12 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    static int N, M;
-
-    static final int MOD = 1_000_000_000;
+class Main{
+    private static final int MOD = 1_000_000_000;
 
     public static void main(String[] args) throws IOException{
-        N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         long[][][] dp = new long[N + 1][10][1 << 10];
 
